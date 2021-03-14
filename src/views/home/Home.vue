e70e55c1dbf7a118846c25d34ed20bcf318066f9
Ryangfeng <2981898270@qq.com>
2021-03-14 10:59:06 +0800

    day01

---
+++ src/views/home/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home">
+    <NavBar class="home-navbar">
+      <div slot="center">购物街</div>
+    </NavBar>
+  </div>
+</template>
+
+
+<script>
+  import NavBar from 'components/common/navbar/NabBar'
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    data() {
+      return {
+       banners:[],
+        recommends:[]
+      }
+    },
+    components: {
+      NavBar
+    },
+    create() {
+      //1.请求多个数据e
+      getHomeMultidata().then(res => {
+        this.banners=res.data.banner.list;
+        this.recommends=res.data.recommend.list;
+      },err=>{
+        console.log(err);
+      })
+    }
+  }
+</script>
+
+
+<style scoped>
+  .home-navbar {
+    background-color: var(--color-tint);
+    color: white;
+  }
+</style>
